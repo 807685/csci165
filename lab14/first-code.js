f0c0d6f3688edb70a4ee9222c4c074c1778f8c91
Aman kumar <807685@columbiacollege.ca>
2020-07-18 16:33:57 -0700

    ..
    Add 14

---
+++ lab14/first-code.js
@@ -0,0 +1,10 @@
+say _hello= function(){//variable
+  count=54;//defining a
+    //variable= value (initilizing)
+    count= count* 2; // variable= new_value
+    // alert (count); // access the variable
+    length = 7.32 //float new_value
+    hello="hello";
+    msg=hello;
+    alert(msg+ 'world'+count);
+  }
