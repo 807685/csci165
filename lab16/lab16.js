8d0c03a5131a93e173e48c9bc961b70b1f70e3f9
Aman kumar <807685@columbiacollege.ca>
2020-08-05 18:28:06 -0700

    ..
    Add

--- lab16/lab16.js
+++ lab16/lab16.js
@@ -44,7 +44,7 @@ jQuery("#outterenterleave").mouseleave(function(){
 
 jQuery("#outertextenterleave").text("mouse leaves");
 n= n+ 1;
-jQuery("#outercounterenter").text(n + "leaves");
+jQuery("#outercounterenterleave").text(n + "leaves");
 });
 //Inner div
 c=0;
