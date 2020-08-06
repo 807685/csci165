5e93ced88c282a5c8ad78cb1271c1f93ac107537
Aman kumar <807685@columbiacollege.ca>
2020-08-05 17:56:51 -0700

    ..
    Add

--- lab16/lab16.js
+++ lab16/lab16.js
@@ -1,65 +1,65 @@
 setup=function(){
 
 t=0
-jQuery ("#outteroverout").mouseover(function(){
+jQuery("#outteroverout").mouseover(function(){
 
-jQuery("#outertextoverout").text ("mouseover");
+jQuery("#outertextoverout").text("mouseover");
 t=t +1;
-jQuery("#outercounterover").text (t + "over");
+jQuery("#outercounterover").text(t + "over");
 });
 
 z=0;
-jQuery ("#outteroverout").mouseover(function(){
+jQuery("#outteroverout").mouseover(function(){
 
-jQuery("#outertextoverout").text ("mouse out");
+jQuery("#outertextoverout").text("mouse out");
 z= z+ 1;
-jQuery("#outercounterout").text (z + "outs");
+jQuery("#outercounterout").text(z + "outs");
 });
 //inner diva
 a=0;
-jQuery ("#inneroverout").mouseover(function(){
+jQuery("#inneroverout").mouseover(function(){
 
-jQuery("#innertextoverout").text ("mouse over");
+jQuery("#innertextoverout").text("mouse over");
 a= a+ 1;
-jQuery("#innercounterover").text (a + "overs");
+jQuery("#innercounterover").text(a + "overs");
 });
  i=0;
-jQuery ("#inneroverout").mouseover(function(){
+jQuery("#inneroverout").mouseover(function(){
 
-jQuery("#innertextoverout").text ("mouse out");
+jQuery("#innertextoverout").text("mouse out");
 i= i+ 1;
-jQuery("#innercounterout").text (i + "outs");
+jQuery("#innercounterout").text(i + "outs");
 });
 //Enter and Leave Events
 //outer div
 d=0;
-jQuery ("#outterenterleave").mousenter(function(){
+jQuery("#outterenterleave").mousenter(function(){
 
-jQuery("#outertextenterleave").text ("mouse enter");
+jQuery("#outertextenterleave").text("mouse enter");
 d=d+ 1;
-jQuery("#OutercounterEnter").text (d + "enters");
+jQuery("#OutercounterEnter").text(d + "enters");
 });
 n=0;
-Query ("#outterenterleave").mouseleave(function(){
+Query("#outterenterleave").mouseleave(function(){
 
-jQuery("#outertextenterleave").text ("mouse leaves");
+jQuery("#outertextenterleave").text("mouse leaves");
 n= n+ 1;
-jQuery("#OutercounterEnter").text (n + "leaves");
+jQuery("#OutercounterEnter").text(n + "leaves");
 });
 //Inner div
 c=0;
-jQuery ("innerenterleave").mousenter(function(){
+jQuery("innerenterleave").mousenter(function(){
 
-jQuery("#innertextenterleave").text ("mouse enter");
+jQuery("#innertextenterleave").text("mouse enter");
 c=c+ 1;
-jQuery("#innercounterEnter").text (c + "enters");
+jQuery("#innercounterEnter").text(c + "enters");
 });
 y=0;
-jQuery ("innerenterleave").mouseleave(function(){
+jQuery("innerenterleave").mouseleave(function(){
 
-jQuery("#innertextenterleave").text ("mouse leave");
+jQuery("#innertextenterleave").text("mouse leave");
 y=y + 1;
-jQuery("#innercounterleave").text (y + "leaves");
+jQuery("#innercounterleave").text(y + "leaves");
 });
 }
-jQuery(document). ready (setup)
+jQuery(document).ready (setup)
