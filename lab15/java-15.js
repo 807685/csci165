b278d9e0bddd6214d08435c4527b9d217ce2eb74
Aman kumar <807685@columbiacollege.ca>
2020-08-10 10:44:07 -0700

    ..
    Add

--- lab15/java-15.js
+++ lab15/java-15.js
@@ -6,10 +6,11 @@ text = jQuery( '#op1').val() + " + " + jQuery('#op2').val() + " = " ;
 jQuery('#result').val(text + result);
 
 styled_result = "<span id = 'addstyle'>" + text + result + "</span>";
-old_div_content =jQuery('#history').html();
-new_div_content = old_div_content + styled_result +'<br>';
-jQuery ('#result').html (text + result ); // or use
-jQuery ('#history').html (new_div_content);
+// old_div_content =jQuery('#history').html();
+// new_div_content = old_div_content + styled_result +'<br>';
+jQuery ('#result').html(text + result ); // or use
+// jQuery ('#history').html(new_div_content);
+jQuery ('#history').append(styled_result +'<br>');
 // or use
                                            // jQuery ('#history'). append (styled_result+ 'br'):
                                           // append will add the new content without overwriting
@@ -25,8 +26,8 @@ jQuery('#result').val(text + result);
 styled_result = "<span id = 'substyle'>" + text + result + "</span>";
 old_div_content =jQuery('#history').html();
 new_div_content = old_div_content + styled_result +'<br>';
-jQuery ('#result').html (text + result );
-jQuery ('#history').html (new_div_content);
+jQuery ('#result').html(text + result );
+jQuery ('#history').html(new_div_content);
     }
 
 
@@ -40,8 +41,8 @@ jQuery('#result').val(text + result);
 styled_result = "<span id = 'divstyle'>" + text + result + "</span>";
 old_div_content =jQuery('#history').html();
 new_div_content = old_div_content + styled_result +'<br>';
-jQuery ('#result').html (text + result);
-jQuery ('#history').html (new_div_content);
+jQuery ('#result').html(text + result);
+jQuery ('#history').html(new_div_content);
     }
 
 mul = function(){
@@ -53,20 +54,35 @@ jQuery('#result').val(text + result);
 styled_result = "<span id = 'mulstyle'>" + text + result + "</span>";
 old_div_content =jQuery('#history').html();
 new_div_content = old_div_content + styled_result +'<br>';
-jQuery ('#result').html (text + result);
-jQuery ('#history').html (new_div_content);
+jQuery ('#result').html(text + result);
+jQuery ('#history').html(new_div_content);
 }
 show= function (){
-jQuery ('#history ').show ()}
-hide= function(){
-jQuery ('#history ').hide (); // or use  // jQuery ('#history'). attr ('style', 'display:none');
+// jQuery('#history').show();}
+jQuery('#history').attr('style', 'display:block');
 }
+hide= function(){
+// jQuery('#history').hide(); // or use  // jQuery ('#history'). attr ('style', 'display:none');
+// jQuery('#history').attr('style', 'display:none');
+jQuery('#history').css('display', 'none');
+}
+
+clear= function(){
+  jQuery('#history').text(' ');
+}
+
+
 setup = function(){
 jQuery('#add_button').click(add);
  jQuery('#sub_button').click(sub);
  jQuery('#mul_button').click(mul);
  jQuery('#div_button').click(div);
+ jQuery('#add_button').mouseenter(add);
+  jQuery('#sub_button').mouseenter(sub);
+  jQuery('#mul_button').mouseenter(mul);
+  jQuery('#div_button').mouseenter(div);
  jQuery('#show_button').click(show);
  jQuery('#hide_button').click(hide);
+ jQuery('#clearbutton').click(clear);
 }
 jQuery(document).ready(setup);
